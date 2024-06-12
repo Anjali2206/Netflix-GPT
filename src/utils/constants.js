2effa16141bbf99201262a9c00cb6bfdaa969ed2
Anjali Pawar <anjalipawar2206@gmail.com>
2024-06-12 16:58:39 +0530

    Responsive Feature

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ export const API_OPTIONS= {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMmY4ZDk3YjFkMGI1YWRmYmZhZGZmYTI4NDU5ZDE5MiIsInN1YiI6IjY2NWVlMmRkMDEzMzk4YzE2ODY1ODE1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Pjsn0wFTrzxQDY4gszhT-o0CRE2dm3S-z6pOI1OiwE0'
+      Authorization: 'Bearer ' + process.env.REACT_APP_TMDB_KEY
     }
   };
   
@@ -19,4 +19,4 @@ export const API_OPTIONS= {
     {identifier:'chinese', name:"Chinese"},
   ]
 
-  export const OPENAI_KEY= process.env.REACT_APP_OPENAI_KEY ? process.env.REACT_APP_OPENAI_KEY : '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'
+  export const OPENAI_KEY= process.env.REACT_APP_OPENAI_KEY
